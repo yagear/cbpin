4156cba8f4e37e9098f485ebe184e4badd51ecfd
Yagear <41774402+yagear@users.noreply.github.com>
2018-08-05 03:21:42 +0900

    Update web.js

--- web.js
+++ web.js
@@ -1,6 +1,13 @@
+var cool = require('cool-ascii-faces');
 var express = require('express');
+var app = express();
 
-var app = express.createServer();
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 
 app.get('/', function(req, res){
 	res.set('Content-Type', 'text/html');
@@ -14,5 +21,6 @@ app.get('/:entity/:command', function(req, res) {
 	res.send('command:' + req.params.command);
 });
 
-var port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
+});
