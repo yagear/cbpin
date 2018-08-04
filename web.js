60166e301c5c9f0c7fba62bf9487209cf78ffd9a
Yagear <41774402+yagear@users.noreply.github.com>
2018-08-05 01:21:25 +0900

    Update web.js

--- web.js
+++ web.js
@@ -2,6 +2,10 @@ var express = require('express');
 
 var app = express.createServer();
 
+app.get('/', function(req, res){
+	res.set('Content-Type', 'text/html');
+	res.send('<html><head></head><body>test</body></html>');
+});
 app.get('/a', function(req, res) {
 	res.send({ user: 'tobi' });
 	res.send([1,2,3]);
