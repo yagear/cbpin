82667ff0dd7ad85f616a446f8ac0714642cbd22a
Yagear <41774402+yagear@users.noreply.github.com>
2018-08-05 01:14:32 +0900

    Update web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer();
 
 app.get('/a', function(req, res) {
-	res.end();
+	res.send({ user: 'tobi' });
+	res.send([1,2,3]);
 });
 
 var port = process.env.PORT || 3000;
